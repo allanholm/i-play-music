ab2e58ad51597cbdb6bcc2cc76478d4ebdf4b7f0
Allan Holm Søndergaard <6605392@rts-365.dk>
2021-01-20 13:45:43 +0100

    featured

--- src/pages/Featured.js
+++ src/pages/Featured.js
@@ -1,14 +1,17 @@
+import FeaturedCard from '../components/featured/FeaturedCard';
 import Heading from '../components/Heading';
 import NavigationBar from '../components/NavigationBar';
 import SiteHeader from '../components/SiteHeader';
+import './Featured.css'
 
 export default function Featured() {
   return (
     <div className="featuredPage">
       <SiteHeader title="Featured"/>
       <Heading title="Featured" />
-      
-      <NavigationBar /> 
+      <FeaturedCard img="https://via.placeholder.com/325x425" title="The Greatest Showman" category="Soundtrack"/>
+      <FeaturedCard img="https://via.placeholder.com/325x425" title="The Greatest Showman" category="Soundtrack"/>
+      <NavigationBar className="navigationBar"/> 
     </div>
   );
 }
