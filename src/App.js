b7e71969cb365f80e45f6a741ec0df92c9cb5156
Allan Holm Søndergaard <6605392@rts-365.dk>
2021-02-02 12:01:29 +0100

    rekt

--- src/App.js
+++ src/App.js
@@ -5,15 +5,19 @@ import Featured from './pages/Featured';
 import Categories from './pages/Categories';
 import Albums from './pages/Albums';
 import AlbumDetails from './pages/AlbumDetails';
+import Playlists from './pages/Playlists';
+import Player from './pages/Player';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Featured path="/"/>
+        <Featured default/>
         <Categories path="/categories" />
         <Albums path="/albums" />
         <AlbumDetails path="/albums/:id" />
+        <Playlists path="/playlists" />
+        <Player path="/player" />
       </Router>
       <NavigationBar className="navigationBar"/> 
     </div>
---
+++ src/pages/Playlists.js
@@ -0,0 +1,12 @@
+import './Playlists.css';
+import SiteHeader from '../components/SiteHeader';
+import Heading from '../components/Heading';
+
+export default function Playlists() {
+  return(
+    <div className="playlistPage">
+      <SiteHeader title="Playlists" />
+      <Heading title="Playlists" />
+    </div>
+  )
+}
